2e8aff1876fa4511da10855a951e7da44d0ee474
LingTim <118987469+LingTim@users.noreply.github.com>
2026-03-10 15:17:51 +0800

    Update countline.sh

--- hw1/LingTim/countline.sh
+++ hw1/LingTim/countline.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+if [ "$#" -eq 0 ]; then
+    echo "missing file name"
+elif [ "$#" -gt 1 ]; then
+    echo "only one argument is allowed"
+else
+    fname="$1"
+    if [ -e "$fname" ]; then
+        lines=$(wc -l < "$fname"
+        echo "$lines lines in $fname"
+    else
+        echo "$fname not found"
+    fi
+fi
