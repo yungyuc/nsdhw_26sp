a5dd9d91530bafa0c219cf599f466b79ad29d466
annie95162 <annie719@gmail.com>
2026-03-11 21:44:42 +0800

    fix line counting

--- hw1/annie95162/countline.py
+++ hw1/annie95162/countline.py
@@ -1,24 +1,23 @@
 #!/bin/sh
 """:"
 if [ -z "$PYTHON_BIN" ]; then
-    PYTHON_BIN=python3
+	PYTHON_BIN=python3
 fi
 
 exec "$PYTHON_BIN" "$0" "$@"
-":"""
+" """
 
 import sys
 import os.path
-
 if len(sys.argv) < 2:
-    sys.stdout.write('missing file name\n')
+	sys.stdout.write('missing file name\n')
 elif len(sys.argv) > 2:
-    sys.stdout.write('only one argument is allowed\n')
+	sys.stdout.write('only one argument is allowed\n')
 else:
-    fname = sys.argv[1]
-    if os.path.exists(fname):
-        with open(fname) as fobj:
-            lines = fobj.readlines()
-        sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
-    else:
-        sys.stdout.write('{} not found\n'.format(fname))
+	fname = sys.argv[1]
+	if os.path.exists(fname):
+		with open(fname) as fobj:
+			count = fobj.read().count('\n')
+		sys.stdout.write('{} lines in {}\n'.format(count, fname))
+	else:
+		sys.stdout.write('{} not found\n'.format(fname))
