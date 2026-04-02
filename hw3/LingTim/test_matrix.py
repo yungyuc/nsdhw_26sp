0b7dd880d71360a86f387b2cfcca8eac065c1c12
LingTim <118987469+LingTim@users.noreply.github.com>
2026-04-02 16:22:53 +0800

    Update test_matrix.py

--- hw3/LingTim/test_matrix.py
+++ hw3/LingTim/test_matrix.py
@@ -5,4 +5,21 @@ def test_basic_creation():
     mat = _matrix.Matrix(10, 10)
     assert mat.nrow == 10
     assert mat.ncol == 10
-    assert mat[0, 0]
+    assert mat[0, 0] == 0.0 
+
+def test_multiplication():
+    size = 100
+    mat1 = _matrix.Matrix(size, size)
+    mat2 = _matrix.Matrix(size, size)
+
+    for i in range(size):
+        for j in range(size):
+            mat1[i, j] = i * 2 + j
+            mat2[i, j] = i + j * 2
+
+    res_naive = _matrix.multiply_naive(mat1, mat2)
+    res_tile = _matrix.multiply_tile(mat1, mat2, 16)
+    res_mkl = _matrix.multiply_mkl(mat1, mat2)
+
+    assert res_naive == res_tile
+    assert res_naive == res_mkl
